0a3d36bf4d01d5189af68de78f2b20f7406939a7
manojpkumarp <manidarling207@gmail.com>
2022-06-03 08:50:18 +0000

    variables changed and committed

--- manoj/variables.tf
+++ manoj/variables.tf
@@ -1,26 +1,26 @@
 variable "rgname" {
     description = "resource group name"
-    default     = "CROME_RG_DEV"
+    default     = "CROME_RG_DEV1"
 }
 variable "location" {
     description = "location name"
     default     = "East Us"
 }
 variable "vnet_name" {
-     description = "name for vnet"
+     description = "name for vnet1"
      type  =   any
-     default     = "CROME_VNET_DEV"
+     default     = "CROME_VNET_DEV1"
 }
 variable "address_space" {
-     default     = ["10.1.0.0/16"]
+     default     = ["10.2.0.0/16"]
      type        = any
 }
 variable "subnet_name" {
-     default     = "CROME_SUBNET_DEV"
+     default     = "CROME_SUBNET_DEV1"
      type        = any
 }
 variable "subnetipaddress" {
-      default     = "10.1.0.0/24"
+      default     = "10.2.0.0/24"
       type        = any
   
 }
